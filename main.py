1109fb7bd0f78cb0a0f8a051231345696e9ca394
mallahsalman06 <sufisalman06@gmail.com>
2021-11-21 14:23:21 +0500

    added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Code by Salman Mallah
+lsOne = [6, 2, 3, 4, 5, 1]
+lsTwo = [1, 2, 3, 4, 5, 6]
+
+
+def addlist(one, two):
+    empty_list = []
+    for i in range(len(one)):
+        empty_list.append(one[i] + two[i])
+    print(empty_list)
+
+
+addlist(lsOne, lsTwo)
