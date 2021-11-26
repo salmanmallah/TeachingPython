997289ce40b18cb5c49664069ddf048e35aa6e8d
salmanpython06 <pythonsalman06@gmail>
2021-11-26 23:34:09 +0500

    work done on this file

---
+++ chapter2/userInput.py
@@ -0,0 +1,11 @@
+"""input() function"""
+"""use to get data from user"""
+# user_age = input('Enter your age: ')
+# print('your age is '+user_age)
+
+"""python script to check Even or Odd number"""
+x = int(input('Enter a number: '))
+if x % 2 == 0:
+    print(x, ' is even number')
+else:
+    print(x, ' is odd number')
