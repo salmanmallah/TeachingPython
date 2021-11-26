e156b975382c4fc5b047af5bde343458c6749e5d
salmanpython06 <pythonsalman06@gmail>
2021-11-26 16:56:02 +0500

    create a patern design

--- pattern.py
+++ pattern.py
@@ -1,2 +1,7 @@
-num = 12
-
+# num = int(input('Enter the number of rows: '))
+num = 5
+for row in range(num):
+    val = row+1
+    for col in range(row+1):
+        print(val, end=" ")
+    print()
