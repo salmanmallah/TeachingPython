e92e82eb94f57946a6b2486c4f9a2cf7ecb6037e
salmanpython06 <pythonsalman06@gmail>
2021-11-26 17:25:48 +0500

    updated

--- percentage.py
+++ percentage.py
@@ -1,9 +1,12 @@
 # calculate percentage of given amount by user.
 totalAmount = int(input('Enter you total amount : '))
 getPercentage = int(input('percentage you want to know: '))
-def Givepercentage(amount,percentage):
+
+
+def Givepercentage(amount, percentage):
     quotients = percentage / amount
     answer = quotients * 100
     print(f'{percentage}% of {amount} is {answer}')
 
-Givepercentage(totalAmount,getPercentage)
+
+Givepercentage(totalAmount, getPercentage)
