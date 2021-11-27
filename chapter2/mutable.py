8c5564d62defd100e4bd3d2f8bc11156d2078d61
salmanpython06 <pythonsalman06@gmail>
2021-11-27 20:38:40 +0500

    work done on this file

---
+++ chapter2/mutable.py
@@ -0,0 +1,16 @@
+"""
+    chapter 2
+    strings are immutable
+    
+    Definition of immutable:
+        - an immutable object is an object whose state can not be modified after it
+          is created. 
+"""
+string = "Python"
+string.replace("P", "G")
+print(string)
+"""
+    output: 
+            Python
+    You can see that string not changed bcz string are  immutable in python     
+"""
