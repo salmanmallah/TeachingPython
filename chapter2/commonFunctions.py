9f352fa7ff19fa8b883287e192d1ac8dd361b99c
salmanpython06 <pythonsalman06@gmail>
2021-11-27 08:17:06 +0500

    practice done on this file.

---
+++ chapter2/commonFunctions.py
@@ -0,0 +1,14 @@
+"""
+    chapter Two
+    Python Functions
+
+    str() function,
+    int() function,
+    float() function,
+"""
+number = 101
+print(type(number))
+
+print(type(str(number)))
+
+print(float(number))
---
+++ chapter2/multiple_variable.py
@@ -0,0 +1,11 @@
+"""
+    chapter Two
+    Python Functions
+
+    More about Variables
+"""
+name, age = 'salman', 21
+print(f'Hello! {name}, Your age is {age}')
+
+x = y = z = 10
+print(x+y+z)
