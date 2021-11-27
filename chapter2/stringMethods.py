ee143ec0f0080191034380ef64e68a60ea1380f6
salmanpython06 <pythonsalman06@gmail>
2021-11-27 17:31:50 +0500

    work done on this file

---
+++ chapter2/stringMethods.py
@@ -0,0 +1,21 @@
+"""
+    chapter two
+    String methods
+
+    len()
+    lower()
+    upper()
+    little()
+    count()
+"""
+
+name = 'SalmAnMaLLaH'
+print(
+    name,
+    '\n1. len() ', len(name),
+    '\n2. lower() ', name.lower(),
+    '\n3. upper() ', name.upper(),
+    '\n4. title() ', name.title(),
+    '\n5. count(a) ', name.lower().count('a')
+
+)
