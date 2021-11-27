f366a8707ac5775084634b7ae3d656ce71494f17
salmanpython06 <pythonsalman06@gmail>
2021-11-27 17:09:46 +0500

    work done on this fie

---
+++ chapter2/string_indexing.py
@@ -0,0 +1,19 @@
+'''
+    chapter 2
+    String Indexing
+'''
+# In python index starts from 0
+# every character in string has a position.
+
+lang = "Sindhi"
+print(lang[::-1])
+
+'''String slicing'''
+print(
+    lang[:5]
+)
+
+'''Step argument'''
+print(
+    lang[::2]
+)
