eace137cddd00131ab2316cda2de7de37cfe0d8f
salmanpython06 <pythonsalman06@gmail>
2021-11-29 00:23:03 +0500

    correct miss spelling

--- chapter3/check_empty_or_not.py
+++ chapter3/check_empty_or_not.py
@@ -2,7 +2,7 @@
     Chapter Three
     check empty or not
 """
-# this thing is very empty.
+# this thing is very important.
 name = input('Enter your name don\'t leave it empty please... : ')
 if name:
     print(bool(name))
