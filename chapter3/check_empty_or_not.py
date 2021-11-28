a4b85afc708aca8260e497c9ff2bdf42d74528b9
salmanpython06 <pythonsalman06@gmail>
2021-11-28 23:38:33 +0500

    important to learn this

---
+++ chapter3/check_empty_or_not.py
@@ -0,0 +1,10 @@
+"""
+    Chapter Three
+    check empty or not
+"""
+# this thing is very empty.
+name = input('Enter your name don\'t leave it empty please... : ')
+if name:
+    print(bool(name))
+else:
+    print(bool(name))
