c823ddc3adcf467ed2405209e9957b2bd32e4deb
salmanpython06 <pythonsalman06@gmail>
2021-12-01 22:31:03 +0500

    work done one this file

---
+++ chapter4/func.py
@@ -0,0 +1,14 @@
+"""
+    CHAPTER 4
+    function Introduction
+    what is a function
+
+    syntax:
+            def func_name(parameters):
+                statements
+"""
+
+def add_three(a, b, c):
+    print(a+b+c)
+
+add_three(3,45,5)
