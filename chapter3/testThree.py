7deb290d602964b53ce8b2ea86f5bbe024fc155c
salmanpython06 <pythonsalman06@gmail>
2021-12-01 21:21:03 +0500

    TypeError:’int’ object is not subscriptable SOLVED

--- chapter3/testThree.py
+++ chapter3/testThree.py
@@ -7,10 +7,16 @@
     *Ask a user for natural number (n)
     *print() total from 1 to 10
 """
-n = int(input('Enter natural number to find sum : '))
-i = 1
+n = input('Enter natural number to find sum : ')
+# i = 1
 sum = 0
-while i <= n:
-    sum += i
-    i += 1
+# while i <= n:
+#     sum += i
+#     i += 1
+# print(sum)
+
+
+# same programme with for loop
+for i in range(len(n)):
+    sum += int(n[i])
 print(sum)
