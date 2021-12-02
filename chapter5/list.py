eca2078ea9008d5eac5e5525cf6303a1dbdccbd0
salmanpython06 <pythonsalman06@gmail>
2021-12-02 05:04:31 +0500

    work done on this file

---
+++ chapter5/list.py
@@ -0,0 +1,22 @@
+"""
+    CHAPTER 5
+    Intro to list
+
+    list
+        -list is a data structure.
+        -ordered collection of items
+        -you can store anything in lists int, float, string
+
+    for example:
+        -you went to store multiples integers in one variable.
+            so how you will do it?
+        -In this case you will use list to store multiple
+         objects in one variable
+
+    *we create list by using square brackets [  ]
+
+    NOTE:
+        lists are mutable in python.
+"""
+number = [1, 2, 3, 4, 5]
+print(number[:2])
