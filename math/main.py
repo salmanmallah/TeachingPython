3f2b82c2309d8105b239a041ad282ff39e87999b
salmanpython06 <pythonsalman06@gmail>
2021-12-07 13:09:04 +0500

    work doen on this file

--- math/main.py
+++ math/main.py
@@ -4,13 +4,18 @@
 """
 
 num = int(input('Enter your number to check prime number or not: '))
-flag = True
+flag = False
 if num > 1:
-    for i in range(2, num+1):
+    for i in range(2, num):
         if (num % i) == 0:
-            flag = False
+            flag = True
             break
-if flag:
+if num == 1:
+    print(f'{num} is not prime number')
+elif flag:
     print(f'{num} is not prime number')
 else:
     print(f'{num} is a prime number')
+
+
+
