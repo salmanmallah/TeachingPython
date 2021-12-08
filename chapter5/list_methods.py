1b132576d7b22ce68b38e608ce19f13b8cddc17e
salmanpython06 <pythonsalman06@gmail>
2021-12-08 15:57:45 +0500

    work done on this fie le

---
+++ chapter5/list_methods.py
@@ -0,0 +1,19 @@
+"""
+    CHAPTER 5
+    - ADD DATA TO LIST
+"""
+animal = ['Cat', 'Dog', 'Elephant']
+
+# append() method
+animal.append('Frog')
+print(animal)
+
+# adding list to a list
+animalTwo = ['Giraffe', 'Horse']
+animal.append(animalTwo)
+print(animal)
+
+# insert() method
+animal.insert(0, 'Alligator',)
+print(animal)
+
