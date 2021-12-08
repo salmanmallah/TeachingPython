a4368b581fa0dae60aa5431e3961f03c4cbd9a98
salmanpython06 <pythonsalman06@gmail>
2021-12-08 16:12:58 +0500

    work done on this file

--- chapter5/list_methods.py
+++ chapter5/list_methods.py
@@ -1,19 +1,34 @@
 """
     CHAPTER 5
-    - ADD DATA TO LIST
+    - METHOD TO ADD DATA IN LIST
 """
 animal = ['Cat', 'Dog', 'Elephant']
 
 # append() method
-animal.append('Frog')
-print(animal)
+# animal.append('Frog')
+# print(animal)
 
 # adding list to a list
 animalTwo = ['Giraffe', 'Horse']
-animal.append(animalTwo)
-print(animal)
+# animal.append(animalTwo)
+# print(animal)
 
 # insert() method
-animal.insert(0, 'Alligator',)
-print(animal)
+animal.insert(0, 'Alligator', )
+# print(animal)
 
+vowels = ['a', 'e', 'i', 'u']
+# vowels.insert(3, 'o')
+# print(vowels)
+
+# concatenate two list by using + operator
+empty_list = animal + vowels
+# print(empty_list)
+
+# extend() method
+# animal.extend(vowels)
+# print(animal)
+
+'''
+    DELETE DATA FROM LIST
+'''
