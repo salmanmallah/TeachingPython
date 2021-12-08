c9728cd5a49b1f3a8ac8eeacfd7d9a1af811519a
salmanpython06 <pythonsalman06@gmail>
2021-12-09 00:46:47 +0500

    work done on this file

--- chapter5/test_two.py
+++ chapter5/test_two.py
@@ -5,11 +5,19 @@
          function will return a reveresed list
 """
 
-def reversed_list(l):
-    return l[::-1]
+# def reversed_list(l):
+#     return l[::-1]
 
-num_list = list(range(1,11))
-salmaninfo = 'salman is the best in the world'.split()
+def reversed_list(l):
+    null = []
+    for i in range(len(l)):
+        null.append(l.pop())
+    return null
+
+
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# salmaninfo = 'salman is the best in the world'.split()
 print(
-    reversed_list(salmaninfo)
+    reversed_list(num_list)
 )
+
