6c7bc0af5e1536656eb8fb2ef1808f3f3828d695
salmanpython06 <pythonsalman06@gmail>
2021-12-12 18:28:23 +0500

    work done on this file

---
+++ chapter7/intro.py
@@ -0,0 +1,18 @@
+"""
+    Introduction to Dictionaries.
+
+    why we use dictionaries?
+    -Because of limitation of lists, list are not enought to represent real data.
+        EXAMPLE:
+            user = ['salman', 20, [fav_movies, 'tarzan'], ['fav_hero', 'superMan']]
+            #this list contains usr's name, age, fav movies, far_hero,
+            #you can do this but this is not good way to do this.
+
+        Q: what are dictionaries?
+        A: Unordered collections of data in [key: value] pair.
+"""
+
+user_info = {'key': 'value', 'name':'salman', 'age': 20}
+print(
+    user_info
+)
