4e0709f08bb71710d0de15e949f6cf41864a7e56
salmanpython06 <pythonsalman06@gmail>
2021-12-16 21:04:07 +0500

    work done this on this file

--- chapter8/set_intro.py
+++ chapter8/set_intro.py
@@ -4,3 +4,7 @@
 
         Defination: unordered collection of unique items.
 """
+s = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+list_one = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5]
+
+print(list(set(list_one)))
