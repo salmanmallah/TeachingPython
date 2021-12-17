7fc11a5802852c6d65ff02c4fb757fe337408c72
salmanpython06 <pythonsalman06@gmail>
2021-12-18 01:47:23 +0500

    work done on this file

--- chapter8/set_intro.py
+++ chapter8/set_intro.py
@@ -4,7 +4,38 @@
 
         Defination: unordered collection of unique items.
 """
-s = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+s = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10}
 list_one = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5]
 
-print(list(set(list_one)))
+# print(list(set(list_one)))
+# print(len(s))
+
+'''
+    # Access Data from SET.
+    # 2 ways to access data from set.
+    #    1. using for loop to access data.
+    #    2. in keyword
+'''
+fruits_set = {'Orange', 'Banana', 'Apple', 'Grapes'}
+for i in fruits_set:
+    print(i)
+
+if 'Grapes' in fruits_set:
+    print('Grapes is present')
+else:
+    print('Gapes is not present')
+
+
+'''
+    # ADD DATA TO SET
+    # Add an item to a set use the add() method.    
+'''
+# fruits_set.add('Watermelon')
+# print(fruits_set)
+
+more_fruites = {'Green Orange', 'Blue Orange', 'Red Orange'}
+# update() method
+fruits_set.update(more_fruites)
+print(
+    fruits_set
+)
