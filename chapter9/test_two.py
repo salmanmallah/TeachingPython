ef47e950ae1f5caaf364989bf7715d8511dccfd6
mallahsalman06 <sufisalman06@gmail.com>
2021-12-21 18:27:27 +0500

    test two done successfuly

--- chapter9/test_two.py
+++ chapter9/test_two.py
@@ -7,8 +7,11 @@
             INPUT === > [True, False, [1, 2, 3], 1, 1.0, 3]
             OUTPUT ===> ['1', '1.0', '3']
 """
-mix = [True, False, [1, 2, 3], 1, 1.0, 3]
-output = [str(i) for i in mix if type(i) == int or type(i) == float]
-print(output)
 
 
+def num_to_str(l):
+    output = [str(i) for i in l if type(i) == int or type(i) == float]
+    return output
+
+
+print(num_to_str([True, False, [1, 2, 3], 1, 1.0, 3]))
