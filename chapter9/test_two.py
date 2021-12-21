473e486eee622f651fc6e39d15bbe1c25e3d4c57
salmanpython06 <pythonsalman06@gmail>
2021-12-21 19:20:52 +0500

    work done on this file

--- chapter9/test_two.py
+++ chapter9/test_two.py
@@ -2,10 +2,6 @@
 <<<<<<< HEAD
     CHAPTER NO 9
     EXERCISE 2
-"""
-=======
-    CHAPTER 9
-    TEST TWO
         NUM TO STRING
         DEFINE A FUNCTION
         EXAMPLE:
@@ -18,6 +14,4 @@ def num_to_str(l):
     output = [str(i) for i in l if type(i) == int or type(i) == float]
     return output
 
-
 print(num_to_str([True, False, [1, 2, 3], 1, 1.0, 3]))
->>>>>>> ef47e950ae1f5caaf364989bf7715d8511dccfd6
