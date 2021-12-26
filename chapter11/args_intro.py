c23db980f6c7a27ce9d8d06dd35fe1a57111b94c
salmanpython06 <pythonsalman06@gmail>
2021-12-26 19:02:22 +0500

    work done on this file

--- chapter11/args_intro.py
+++ chapter11/args_intro.py
@@ -6,4 +6,15 @@
 # * operator
 # *args
 
-def total
+def total(a, b):
+    return a + b
+# print(total(10, 10)) # but we cannot pass more than two parameters
+
+''' NOW BY USING *ARGS TECHNIQUE WE CAN SOLVE ABOVE PROBLEM '''
+def arg_total(*args):
+    total = 0
+    for i in args:
+        total += i
+    return total
+
+print(arg_total(10, 10, 10, 10, 10, 10, 10))
