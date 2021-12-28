7398880e5327d7d1aee779b428173e9d3fa6599d
salmanpython06 <pythonsalman06@gmail>
2021-12-29 04:06:05 +0500

    work done on this file

--- chapter11/test_one.py
+++ chapter11/test_one.py
@@ -6,14 +6,14 @@ def power_three(user_input, *args):
     product_list = [i**user_input for i in args]
     return product_list
 
-user_input = int(input('enter number: '))
-num_list = [1, 2, 3]
-print(power_three(user_input, *num_list))
-#
-# def power_three(num, *args):
-#     if args:
-#         return [i**num for i in args]
-#     else:
-#         return 'Sorry list is empty'
-#
-# print(power_three(3,*[1, 2, 3]))
+# user_input = int(input('enter number: '))
+# num_list = [1, 2, 3]
+# print(power_three(user_input, *num_list))
+
+def power_three(num, *args):
+    if args:
+        return [i**num for i in args]
+    else:
+        return 'Sorry list is empty'
+
+print(power_three(3,*[1, 2, 3]))
