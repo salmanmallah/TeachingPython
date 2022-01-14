93eeb7caf5882ddbf31fa46984ba9c504018208d
mallahsalman06 <sufisalman06@gmail.com>
2022-01-15 04:33:12 +0500

    done

--- chapter16/test_one.py
+++ chapter16/test_one.py
@@ -16,19 +16,20 @@ class Laptop:
         self.brand = brand_name
         self.model = model_name
         self.price = price
-        self.brand_details = f'Brand : {brand_name}\nModel : {model_name}\nPrice : {price}'
+        self.brand_details = f'Brand : {brand_name}\nModel : {model_name}\nPrice : {price}\n\n\n'
 
 
 laptop_one = Laptop('Dell', 'latitude E5540', 35000)
 laptop_two = Laptop('Hp', 'Envy 13', 100000)
 
-
 # object one
 print(laptop_two.brand_details)
-# print(laptop_one.model)
-# print(laptop_one.price)
-#
-# # object two
-# print('\n', laptop_two.brand)
-# print(laptop_two.model)
-# print(laptop_two.price)
+print(laptop_two.brand)
+print(laptop_one.model)
+print(laptop_one.price)
+
+# object two
+print('\n\n')
+print(laptop_two.brand)
+print(laptop_two.model)
+print(laptop_two.price)
