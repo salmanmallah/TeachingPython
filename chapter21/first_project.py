e6fc24120cbc6e98257af6fd5ea02a6736a4379f
salmanpython06 <pythonsalman06@gmail>
2022-02-18 00:01:09 +0500

    done

--- chapter21/first_project.py
+++ chapter21/first_project.py
@@ -5,3 +5,29 @@
     Python Project One
     By: SALMAN MALLAH
 """
+import os
+import shutil
+
+os.chdir(r'C:\Users\CCS LAPTOP HYD\Desktop\python tutorials')
+path = r'C:\Users\CCS LAPTOP HYD\Desktop\python tutorials'
+
+dict_extension = {
+    'audio_extensions': ('.mp3', '.m4a', '.wav', '.flac'),
+    'video_extensions': ('.mp4', '.mkv', '.MKV', '.flv', '.mpeg'),
+    'document_extension': ('.docx', '.pdf', '.txt')
+}
+
+
+def file_finder(folder_path, file_extension):
+    files = []
+    for file in os.listdir(folder_path):
+        if file.endswith(file_extension):
+            files.append(file)
+    return files
+
+
+for key, value in dict_extension.items():
+    print(file_finder(path,value))
+# for extension_type, extension_value in dict_extension.items():
+#     print(f'Calling file_finder ...')
+#     print(file_finder(path, extension_value))
