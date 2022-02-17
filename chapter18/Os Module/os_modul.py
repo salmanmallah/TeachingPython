6c96278cb92a93298853a13efffd3cf3d2596fac
salmanpython06 <pythonsalman06@gmail>
2022-02-17 09:18:03 +0500

    work done

--- chapter18/Os Module/os_modul.py
+++ chapter18/Os Module/os_modul.py
@@ -5,13 +5,26 @@
 """
 import os
 # print(os.getcwd())
-os.mkdir('subfolder')
+# os.mkdir('subfolder')
 # os.rmdir('subfolder')
-# print(os.path.exists('subfolder'))
+# print(path.exists('Movies'))
 
 # check that folder is exists or not
-if os.path.exists('subfolder'):
-    print('Folder is already created')
-else:
-    # if not present than create below folder
-    os.mkdir('subfolder')
+# if path.exists('Movies'):
+#     print('Folder is already created')
+# else:
+#     # if not present than create below folder
+#     mkdir('Movies')
+#
+# # rmdir('Movies')
+# print(path.exists('Movies'))
+
+# to create file
+# open('uncreate.txt', 'a').close()
+
+# to delete file
+# os.remove('uncreate.txt')
+
+# to change dir
+os.chdir('..')
+print(os.listdir())
