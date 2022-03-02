bdaf606bc92cb8ca0d22afd97db06d2f733bd975
salmanpython06 <pythonsalman06@gmail>
2022-03-03 04:52:28 +0500

    done

--- chapter22/forloopwithWidgets/create_widgets_using_forloop.py
+++ chapter22/forloopwithWidgets/create_widgets_using_forloop.py
@@ -5,10 +5,36 @@ win = tk.Tk()
 win.title("widgets With For loops")
 win.geometry('400x200')
 
-labels = ['Enter your Name: ', 'Enter your Email: ', 'Enter your Age: ']
+# Create Labels
 
+labels = ['Enter your Name: ', 'Enter your Email: ', 'Enter your Age: ']
 for label in range(len(labels)):
-    ttk.Label(win, text=labels[label]).grid(row=label, column=0, sticky=tk.W)
-    ttk.Entry(win).grid(row=label, column=1)
+    current_label = 'label' + str(label)
+    current_label = ttk.Label(win, text=labels[label])
+    current_label.grid(row=label, column=0, sticky=tk.W)
+
+# Create EntryBox
+userinfo = {
+    'name': tk.StringVar(),
+    'email': tk.StringVar(),
+    'age': tk.StringVar()
+}
+counter = 0
+for i in userinfo:
+    cur_entrybox = i + '_entry'
+    cur_entrybox = ttk.Entry(win, textvariable=userinfo[i])
+    cur_entrybox.grid(row=counter, column=1)
+    counter += 1
+
+
+# Submit Button
+def action():
+    print(userinfo['name'].get())
+    print(userinfo['email'].get())
+    print(userinfo['age'].get())
+
+
+ttk.Button(win, text='Click Me', command=action).grid(row=3)
+
 
 win.mainloop()
