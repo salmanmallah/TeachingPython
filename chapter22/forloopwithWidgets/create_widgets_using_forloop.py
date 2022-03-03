a98621a865c6eca6da82cb2f64ae2d51bbf9afe9
mallahsalman06 <sufisalman06@gmail.com>
2022-03-03 06:13:21 +0500

    done

--- chapter22/forloopwithWidgets/create_widgets_using_forloop.py
+++ chapter22/forloopwithWidgets/create_widgets_using_forloop.py
@@ -7,7 +7,8 @@ win.geometry('400x200')
 
 # Create Labels
 
-labels = ['Enter your Name: ', 'Enter your Email: ', 'Enter your Age: ']
+labels = ['Enter your Name: ', 'Enter your Email: ', 'Enter your Age: ', 'Your Country : ', 'Your Province : ',
+          'Your City', 'Your Number : ']
 for label in range(len(labels)):
     current_label = 'label' + str(label)
     current_label = ttk.Label(win, text=labels[label])
@@ -17,11 +18,15 @@ for label in range(len(labels)):
 userinfo = {
     'name': tk.StringVar(),
     'email': tk.StringVar(),
-    'age': tk.StringVar()
+    'age': tk.StringVar(),
+    'country': tk.StringVar(),
+    'province': tk.StringVar(),
+    'city': tk.StringVar(),
+    'number': tk.StringVar()
 }
 counter = 0
 for i in userinfo:
-    cur_entrybox = i + '_entry'
+    # cur_entrybox = i + '_entry'
     ttk.Entry(win, textvariable=userinfo[i]).grid(row=counter, column=1)
     counter += 1
 
@@ -31,9 +36,13 @@ def action():
     print(userinfo['name'].get())
     print(userinfo['email'].get())
     print(userinfo['age'].get())
+    print(userinfo['country'].get())
+    print(userinfo['province'].get())
+    print(userinfo['city'].get())
+    print(userinfo['number'].get())
 
 
-ttk.Button(win, text='Click Me', command=action).grid(row=3)
+ttk.Button(win, text='Click Me', command=action).grid(row=7)
 
 
 win.mainloop()
