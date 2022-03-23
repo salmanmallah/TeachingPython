f10e9ca22ca8b1d1b7c1c330b701fb93d75052c5
mallahsalman06 <sufisalman06@gmail.com>
2022-03-23 18:15:13 +0500

    done

--- chapter23 final project/vpad_text_editor/vpad.py
+++ chapter23 final project/vpad_text_editor/vpad.py
@@ -82,7 +82,7 @@ tool_bar = ttk.Label(main_application)
 tool_bar.pack(side=tk.TOP, fill=tk.X, padx=5)
 
 # Font Box
-font_tuple = tk.font.families()
+font_tuple = font.families()
 font_family = tk.StringVar()
 font_box = ttk.Combobox(tool_bar, width=30, textvariable=font_family, state='readonly')
 font_box['values'] = font_tuple
@@ -511,7 +511,7 @@ main_application.bind('<Control-n>', new_file)
 main_application.bind('<Control-o>', open_file)
 main_application.bind('<Control-s>', save_file)
 main_application.bind('<Control-Alt-s>', save_as)
-main_application.bind('<Control-q>', exit_func)
-main_application.bind('<Control-f>', find_func)
+
+
 
 main_application.mainloop()
