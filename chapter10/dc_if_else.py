496138f36c1c859f7f7f0b09e5f08488dea14a64
mallahsalman06 <sufisalman06@gmail.com>
2022-04-11 21:08:36 +0500

    odd-even

--- chapter10/dc_if_else.py
+++ chapter10/dc_if_else.py
@@ -4,5 +4,11 @@
 """
 # {1:odd, 2;even, 3:odd, 4:even, 5:odd, 6:even}
 
-odd_even_dict = {i:('even'if i % 2 == 0 else 'odd') for i in range(1,11)}
-print(odd_even_dict)
+# odd_even_dict = {i:('even'if i % 2 == 0 else 'odd') for i in range(1,11)}
+# print(odd_even_dict)
+
+
+odd_even = {i: ('Even' if i % 2 == 0 else 'Odd') for i in range(1,11)}
+print(odd_even)
+for i in odd_even:
+    print(odd_even[i])
