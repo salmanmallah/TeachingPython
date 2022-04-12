48253bfb456aeffda4df43ad57cca56b819c274f
mallahsalman06 <sufisalman06@gmail.com>
2022-04-12 21:50:52 +0500

    done

--- ruff/Tkinter Programming/theme.py
+++ ruff/Tkinter Programming/theme.py
@@ -7,25 +7,29 @@ root.geometry('1350x700+0+0')
 root.config(background='#283593')
 
 # Color Chooser Button
-color_button = tk.Button(root, text='ColorChoose', background='#5f5fc4', fg='#ffffff', padx='25', pady='25', command=lambda: choose_theme())
-color_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+# color_button = tk.Button(root, text='ColorChoose', background='#5f5fc4', fg='#ffffff', padx='25', pady='25', command=lambda: choose_theme())
+# color_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 
-def choose_theme():
-    rgb_value = colorchooser.askcolor()
-    hex_color = rgb_value[1]
-    # print(hex_color)
+# def choose_theme():
+#     rgb_value = colorchooser.askcolor()
+#     hex_color = rgb_value[1]
+# print(hex_color)
 
-    # def opposite_color(Hex):
-    #     if Hex[0] == '#':
-    #         Hex = Hex[1:]
-    #     rgb = (Hex[0:2], Hex[2:4], Hex[4:6])
-    #     comp = ['%02x' % (255 - int(a, 16)) for a in rgb]
-    #     return ''.join(comp)
+# def opposite_color(Hex):
+#     if Hex[0] == '#':
+#         Hex = Hex[1:]
+#     rgb = (Hex[0:2], Hex[2:4], Hex[4:6])
+#     comp = ['%02x' % (255 - int(a, 16)) for a in rgb]
+#     return ''.join(comp)
 
-    # set_button_color = opposite_color(hex_color)
-    root.config(background=rgb_value[1])
-    # color_button.config(background='#' +set_button_color)
+# set_button_color = opposite_color(hex_color)
+# root.config(background=rgb_value[1])
+# color_button.configs(background='#' +set_button_color)
+
+
+main_menu = tk.Menu(root, tearoff=False)
+main_menu = tk.Menu(root, tearoff=False)
 
 
 root.mainloop()
