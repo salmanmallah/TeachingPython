fec588f45183f274f9a215a88a3b4f57ccfef212
mallahsalman06 <sufisalman06@gmail.com>
2022-04-16 20:08:38 +0500

    done

--- chapter24/tkinter_mysql.py
+++ chapter24/tkinter_mysql.py
@@ -33,20 +33,25 @@ phone_entry.place(x=200, y=90)
 
 # submit button
 submit = tk.Button(root, text='Create', width=60, command=lambda: insert())
-submit.config(background='green', height=2)
+submit.config(background='green', height=2, foreground='yellow')
 submit.place(x=20, y=120)
 
 # Delete Button
 delete_button = tk.Button(root, text='Delete', width=20, command=lambda: delete())
-delete_button.config(background='red')
+delete_button.config(background='red', foreground='white')
 delete_button.place(x=470, y=30)
 
 # Update Button
 update_button = tk.Button(root, text='Update', width=20, command=lambda: update())
-update_button.config(background='yellow')
+update_button.config(background='yellow', foreground='red')
 update_button.place(x=470, y=60)
 
+# GET BUTTON
+get_button = tk.Button(root, text='GET', width=20, command=lambda: get())
+get_button.config(background='Blue', foreground='white')
+get_button.place(x=470, y=90)
 
+# ############################# USER-MADE FUNCTIONS #################################
 # function to insert data in Mysql
 def insert():
     id_ = id_var.get()
@@ -112,6 +117,27 @@ def update():
         phone_entry.delete(0, tk.END)
 
         messagebox.showinfo('Status', 'Successfully updated!')
+
+def get():
+    global name_entry
+    global phone_entry
+
+    id_ = id_var.get()
+    if id_ == '':
+        messagebox.showwarning('input status', 'Please enter your id to fetch data')
+    else:
+        con = mysql.connect(host='localhost', user='root', password='', database='python-tkinter')
+        cursor = con.cursor()
+        cursor.execute('SELECT * FROM student WHERE id="' + id_ + '" ')
+        tables = cursor.fetchall()
+        # counter = 0
+
+        name_entry.delete(0, tk.END)
+        phone_entry.delete(0, tk.END)
+
+        for rows in tables:
+            name_entry.insert(0, rows[1])
+            phone_entry.insert(0, rows[2])
 
 
 root.mainloop()
